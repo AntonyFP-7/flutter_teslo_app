8df95278226b8f98cdb0b24c1f8bef6b2cce7deb
AntonyFloresPaz <developer4@traxpark.us>
2025-04-09 15:07:34 -0500

    `feat: Added shared preferences functionality and updated auth provider to use it`

--- lib/features/auth/presentation/providers/auth_provider.dart
+++ lib/features/auth/presentation/providers/auth_provider.dart
@@ -1,22 +1,31 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:teslo_app/features/auth/domain/domain.dart';
 import 'package:teslo_app/features/auth/infrastructure/infrastructure.dart';
+import 'package:teslo_app/features/shared/infrastructure/services/key_value_storage_service.dart';
+import 'package:teslo_app/features/shared/infrastructure/services/key_value_storage_service_impl.dart';
 
 enum AuthStatus { checking, authenticated, notAuthenticated }
 
 final authProvider = StateNotifierProvider<AuthNotifier, AuthState>((ref) {
   final authRepository = AutRepositoryEmpl();
+  final keyValueStorageService = KeyValueStorageServiceImpl();
+  
   return AuthNotifier(
     authRepository: authRepository,
+    keyValueStorageService: keyValueStorageService,
   );
 });
 
 class AuthNotifier extends StateNotifier<AuthState> {
+  final KeyValueStorageService keyValueStorageService;
   final AuthRepository authRepository;
 
   AuthNotifier({
     required this.authRepository,
-  }) : super(AuthState());
+    required this.keyValueStorageService,
+  }) : super(AuthState()) {
+    checkAuthStatus();
+  }
   Future<void> loginUser(String email, String password) async {
     await Future.delayed(const Duration(microseconds: 500));
 
@@ -46,6 +55,8 @@ class AuthNotifier extends StateNotifier<AuthState> {
   void checkAuthStatus() async {}
 
   Future<void> logout([String? errorMessage]) async {
+    await keyValueStorageService.removeKey('token');
+
     //todo limpiar token
     state = state.copyWith(
         authStatus: AuthStatus.notAuthenticated,
@@ -53,11 +64,13 @@ class AuthNotifier extends StateNotifier<AuthState> {
         errorMessage: errorMessage);
   }
 
-  void _setLoggedUser(User user) {
-//todo: necesito guardar el token en el dispsitivo
+  void _setLoggedUser(User user) async {
+    await keyValueStorageService.setKeyValue('token', user.token);
+    //todo: necesito guardar el token en el dispositivo
     state = state.copyWith(
       user: user,
       authStatus: AuthStatus.authenticated,
+      errorMessage: '',
     );
   }
 }
